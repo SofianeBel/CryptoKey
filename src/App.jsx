f1b934725fb3788655e8d07b52b19591920ef37a
Sifly <118746369+SofianeBel@users.noreply.github.com>
2024-12-02 14:18:08 +0100

    Version updated to 0.5.0, minimum password length increased to 14 characters and password strength alert added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import React, { useState } from 'react';
 import PasswordGenerator from './components/PasswordGenerator/PasswordGenerator';
 import PasswordOptions from './components/PasswordGenerator/PasswordOptions';
 import PasswordStrength from './components/PasswordGenerator/PasswordStrength';
+import { Alert } from '@mui/material';
 
 function App() {
   const [password, setPassword] = useState('');
-  const [length, setLength] = useState(6);
+  const [length, setLength] = useState(14);
   const [options, setOptions] = useState({
     includeLowercase: true,
     includeUppercase: true,
     includeNumbers: true,
     includeSymbols: true,
   });
+
+   // Déterminez la force du mot de passe
+   const getPasswordStrength = () => {
+    // Logique pour déterminer la force du mot de passe
+    if (password.length < 8) return 'faible';
+    if (password.length < 12) return 'moyen';
+    return 'fort';
+  };
+
+  const strength = getPasswordStrength();
+
 
   return (
     <div className="App">
@@ -28,6 +40,11 @@ function App() {
         setPassword={setPassword}
       />
       <PasswordStrength password={password} />
+      {strength === 'faible' && (
+        <Alert severity = "warning">
+                  Le mot de passe est faible. Veuillez le renforcer pour plus de sécurité.
+                  </Alert>
+                )}
     </div>
   );
 }
